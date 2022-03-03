ee7c97cec16c3fd5295b1af609a1776c93532607
Edivan de Castro <edivandecastro@gmail.com>
2022-03-03 19:53:27 -0300

    Configured factory_bot with rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,6 +39,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.include FactoryBot::Syntax::Methods
+
+  config.order = :rand
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
