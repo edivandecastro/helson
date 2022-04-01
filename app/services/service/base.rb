60c9d7e86595fc865fa38edf0929001151b0d3a9
Edivan de Castro <edivandecastro@gmail.com>
2022-04-01 14:40:09 -0300

    Changed initalize of context in base class

--- app/services/service/base.rb
+++ app/services/service/base.rb
@@ -2,9 +2,12 @@ module Service
   class Base
     attr_accessor :context
 
+    def initialize(context=nil)
+      self.context = context
+    end
+
     def self.call(*args)
-      service = self.new
-      service.context = Service::Context.new(*args)
+      service = self.new(Service::Context.build(*args))
       service.call
     end
   end
