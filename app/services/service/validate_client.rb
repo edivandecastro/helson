aa8d8ab01dfa7805c99d3369730cbb826a2f3ac7
Edivan de Castro <edivandecastro@gmail.com>
2022-04-01 13:17:00 -0300

    Improved service ValidadeClient and your test

--- app/services/service/validate_client.rb
+++ app/services/service/validate_client.rb
@@ -1,10 +1,22 @@
+# frozen_string_literal: true
+
 module Service
   class ValidateClient < Base
     def call
-      app = Application.find_by(client_id: context.client_id)
-      context.client_valid = app ? true : false
-      context.fail(error: "The OAuth client was not found.") if app.nil?
+      context.app = Application.find_by(client_id: context.client_id)
+      context.client_valid = context.app ? true : false
+      context.fail(error) if context.app.nil?
       context
+    end
+
+    private
+
+    def error
+      {
+        status: 401, # move to constant
+        type: :invalid_client, # move to constant
+        message: 'The OAuth client was not found.', # move translate
+      }
     end
   end
 end
--- spec/services/service/validate_client_spec.rb
+++ spec/services/service/validate_client_spec.rb
@@ -1,28 +1,45 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Service::ValidateClient, type: :model do
-  describe "#call" do
-    context "when client was not found" do
-      it "return context failure" do
+  describe '.call' do
+    context 'when client was not found' do
+      it 'return context failure' do
         context = Service::ValidateClient.call(client_id: 1)
         expect(context.failure?).to be true
       end
 
-      it "return message error" do
+      it 'return message with error' do
         context = Service::ValidateClient.call(client_id: 1)
-        expect(context.error).to eq "The OAuth client was not found."
+        expect(context.message).to eq 'The OAuth client was not found.'
+      end
+
+      it 'return status with 401' do
+        context = Service::ValidateClient.call(client_id: 1)
+        expect(context.status).to eq 401
+      end
+
+      it 'return type with :invalid_client' do
+        context = Service::ValidateClient.call(client_id: 1)
+        expect(context.type).to eq :invalid_client
+      end
+
+      it 'return client_valid with false' do
+        context = Service::ValidateClient.call(client_id: 1)
+        expect(context.client_valid).to eq false
       end
     end
 
-    context "when client was found" do
+    context 'when client was found' do
       let(:application) { create(:application) }
 
-      it "return context success" do
+      it 'return context success' do
         context = Service::ValidateClient.call(client_id: application.client_id)
         expect(context.success?).to be true
       end
 
-      it "client_valid is true" do
+      it 'client_valid is true' do
         context = Service::ValidateClient.call(client_id: application.client_id)
         expect(context.client_valid).to be true
       end
