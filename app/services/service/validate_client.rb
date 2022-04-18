92c7ab081ced6c9a9fa2a56191f9fba182bb5aba
Edivan de Castro <edivandecastro@gmail.com>
2022-04-17 22:33:10 -0300

    Refactory in ValidateClient class

--- app/services/service/validate_client.rb
+++ app/services/service/validate_client.rb
@@ -3,20 +3,27 @@
 module Service
   class ValidateClient < Base
     def call
-      context.app = Application.find_by(client_id: context.client_id)
-      context.client_valid = context.app ? true : false
-      context.fail(error) if context.app.nil?
-      context
+      validate_parameter_client_id
+      return context if context.failure?
+
+      validate_client
+      return context if context.client_valid
+
+      context.class_error = Service::Error::InvalidClient
+      Service::ThrowError.call(context)
     end
 
     private
 
-    def error
-      {
-        status: 401, # move to constant
-        type: :invalid_client, # move to constant
-        message: 'The OAuth client was not found.', # move translate
-      }
+    def validate_parameter_client_id
+      context.required_parameter = :client_id
+      Service::ValidateParameter.call(context)
+      context.client_valid = context.success?
+    end
+
+    def validate_client
+      context.app = Application.find_by(client_id: context.client_id)
+      context.client_valid = context.app.present?
     end
   end
 end
--- spec/services/service/validate_client_spec.rb
+++ spec/services/service/validate_client_spec.rb
@@ -5,28 +5,73 @@ require 'rails_helper'
 RSpec.describe Service::ValidateClient, type: :model do
   describe '.call' do
     context 'when client was not found' do
+      let(:context) { Service::ValidateClient.call(client_id: 1) }
+
       it 'return context failure' do
-        context = Service::ValidateClient.call(client_id: 1)
         expect(context.failure?).to be true
       end
 
       it 'return message with error' do
-        context = Service::ValidateClient.call(client_id: 1)
         expect(context.message).to eq 'The OAuth client was not found.'
       end
 
       it 'return status with 401' do
-        context = Service::ValidateClient.call(client_id: 1)
         expect(context.status).to eq 401
       end
 
       it 'return type with :invalid_client' do
-        context = Service::ValidateClient.call(client_id: 1)
         expect(context.type).to eq :invalid_client
       end
 
       it 'return client_valid with false' do
-        context = Service::ValidateClient.call(client_id: 1)
+        expect(context.client_valid).to eq false
+      end
+    end
+
+    context 'when parameter client_id is empty' do
+      let(:context) { Service::ValidateClient.call(client_id: '') }
+
+      it 'return context failure' do
+        expect(context.failure?).to be true
+      end
+
+      it 'return message with error "Missing required parameter: client_id"' do
+        expect(context.message).to eq 'Missing required parameter: client_id'
+      end
+
+      it 'return status with 400' do
+        expect(context.status).to eq 400
+      end
+
+      it 'return type with :invalid_request' do
+        expect(context.type).to eq :invalid_request
+      end
+
+      it 'return client_valid with false' do
+        expect(context.client_valid).to eq false
+      end
+    end
+
+    context 'when parameter client_id is nil' do
+      let(:context) { Service::ValidateClient.call(client_id: nil) }
+
+      it 'return context failure' do
+        expect(context.failure?).to be true
+      end
+
+      it 'return message with error "Missing required parameter: client_id"' do
+        expect(context.message).to eq 'Missing required parameter: client_id'
+      end
+
+      it 'return status with 400' do
+        expect(context.status).to eq 400
+      end
+
+      it 'return type with :invalid_request' do
+        expect(context.type).to eq :invalid_request
+      end
+
+      it 'return client_valid with false' do
         expect(context.client_valid).to eq false
       end
     end
