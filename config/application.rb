b35aee768da7ad44a0998eac4f86417fe072e914
Edivan de Castro <edivandecastro@gmail.com>
2023-09-25 19:02:53 -0300

    Config i18n

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Helson
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = "pt-BR"
+    config.i18n.available_locales = %w(pt-BR en)
   end
 end
